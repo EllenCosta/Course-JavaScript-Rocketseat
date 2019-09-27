21bfeea8f9718ebd9f3a9352cc83074cac200e9a
EllenCosta <55865232+EllenCosta@users.noreply.github.com>
2019-09-27 16:11:47 -0300

    Update ex05.js

--- Modulo1/Exercicio5/ex05.js
+++ Modulo1/Exercicio5/ex05.js
@@ -1,17 +1,29 @@
+//Adicionando sexo
 var usuarios = [
     {
         nome: "Diego",
+        sexo: "M",
         habilidades: ["Javascript", "ReactJS", "Redux"]
     },
     {
         nome: "Gabriel",
+        sexo: "M",
         habilidades: ["VueJS", "Ruby on Rails", "Elixir"]
+    },
+    {
+        nome: "Ellen",
+        sexo: "F",
+        habilidades: ["HTML e CSS", "PHP", "Python", "Javascript"]
     }
 ];
 function msg(usuarios) {
     for (i of usuarios) {
-        console.log(`O ${i.nome} possui as habilidades: `+
-        `${i.habilidades.join(', ')}`);
+        if(i.sexo == 'F') { // Se o sexo for F → o artigo definido será A
+            var art = 'A'
+        } else { // Caso contrário → será O
+            var art = 'O'
+        }
+        console.log(`${art} ${i.nome} possui as habilidades: ${i.habilidades.join(', ')}`);
     }
 }
 
