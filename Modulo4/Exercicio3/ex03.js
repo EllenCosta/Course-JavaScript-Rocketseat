61b1c4a600cd44d04bb63abc3c4bda6710c66275
Ellen Costa <contato.ellenjulian@gmail.com>
2019-10-02 00:40:01 -0300

    Update ex03.js

---
+++ Modulo4/Exercicio3/ex03.js
@@ -0,0 +1,101 @@
+// referenciar div
+var divApp = document.querySelector('#app');
+// criar input
+var campo = document.createElement('input');
+// tipo texto
+campo.setAttribute('type', 'text');
+// name user
+campo.setAttribute('name', 'user');
+// adicionando campo na div
+divApp.appendChild(campo);
+// criar botão
+var btn = document.createElement('button');
+// texto do botão
+var txtbnt = document.createTextNode('Adicionar');
+// posicionando texto dentro do botão
+btn.appendChild(txtbnt);
+// adicionando botão na div
+divApp.appendChild(btn);
+// margem da esquerda de 5px
+btn.style.marginLeft = '5px';
+// criando lista não ordenada (ul)
+var ul = document.createElement('ul');
+// adicionando ul na div
+divApp.appendChild(ul);
+var nome = '';
+btn.onclick = function() {
+    // pegando valor digitado no campo
+    var usuario = campo.value;
+    // chamar a função carrengando
+    carregando();
+    // pegar a url da api com a biblioteca axio
+    axios.get(`https://api.github.com/users/${usuario}/repos`)
+        .then(function(response) {
+            // capturando nome do dono do repositório
+            nome = response.data[0].owner.login;
+            // capturando os repositórios
+            busca(response.data);
+            // limpando o campo
+            campo.value = '';
+            // focando no campo
+            campo.focus();
+        })
+        .catch(function(error) {
+            // alertando erro
+            carregandoErro();
+            // limpando o campo
+            campo.value = '';
+            // focando no campo
+            campo.focus();
+        });
+}
+
+function carregando() {
+    // limpando a lista
+    ul.innerHTML = '';
+    // criando li
+    var li = document.createElement('li');
+    // texto do li
+    var txtli = document.createTextNode('Carregando...');
+    // posicionando texto dentro do li
+    li.appendChild(txtli);
+    // adicionar li dentro da ul
+    ul.appendChild(li);
+}
+
+function carregandoErro() {
+    // limpando a lista
+    ul.innerHTML = '';
+    // pegando valor digitado no campo
+    var usuario = campo.value;
+    // verificando erro
+    var erro = !usuario ? "Campo Obrigatório":"Usuário/Repositório inexistente";
+    // criando item de lista
+    var li = document.createElement('li');
+    // criando texto de erro 
+    var txt = document.createTextNode(erro);
+    // adicionando texto no li
+    li.appendChild(txt);
+    // adicionando li na lista
+    ul.appendChild(li);
+}
+
+// criando a função para preencher os itens da lista
+function busca(repositorio) {
+    // limpando o interior da lista
+    ul.innerHTML = '';
+    // percorrer os dados (para cada item do dado)
+    for (repos of repositorio) {
+        // criando itens de lista (li)
+        var li = document.createElement('li');
+        // texto do item → nomes de cada repositório (na api é referenciado por 'name')
+        var txtli = document.createTextNode(repos.name);
+        // posicionar texto dentro do li
+        li.appendChild(txtli);
+        // adicionando li dentro da ul
+        ul.appendChild(li);
+        // colocando divisória
+        var hr = document.createElement('hr');
+        ul.appendChild(hr);
+    }
+}
