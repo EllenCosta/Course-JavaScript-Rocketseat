ec940d5b4e4e238351320c0702580d50d045bb70
Ellen Costa <contato.ellenjulian@gmail.com>
2019-10-02 00:25:54 -0300

    Maior de 18

---
+++ Modulo4/Exercicio1/ex01.js
@@ -0,0 +1,15 @@
+function checaIdade(idade) {
+    // Retornar uma promise
+    return new Promise(function(resolve, reject) {
+        setTimeout( function() {
+            idade >= 18 ? resolve() : reject();
+        }, 2000)
+    });
+}
+checaIdade(20)
+ .then(function() {
+     console.log("Maior que 18");
+ })
+ .catch(function() {
+     console.log("Menor que 18");
+ });
